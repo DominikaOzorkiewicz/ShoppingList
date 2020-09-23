afceedf14ed112c5e708d54b1fe90a3de0918d28
DominikaOzorkiewicz <62950674+DominikaOzorkiewicz@users.noreply.github.com>
2020-09-23 21:04:41 +0200

    Basic layout with form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './scss/main.scss';
+import {Header} from "./components/Header";
+import {NewItemForm} from "./components/NewItemForm";
 
-function App() {
+const App = () => {
+    const [items, setItems] = useState([]);
+
+    // Add new product to items array
+    const addProduct = (product) => {
+        setItems([...items, product]);
+    }
+
     return (
         <div className="App">
-
+            <Header />
+            <NewItemForm addProductFn={addProduct} />
         </div>
     );
 }
