d54ee763a18467bf8750fc6b80d46ee941522e5c
DominikaOzorkiewicz <62950674+DominikaOzorkiewicz@users.noreply.github.com>
2020-10-19 21:38:11 +0200

    gh-pages

---
+++ precache-manifest.d719e881b34f4bf1318b980cae28145c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f622942cd8655ae13e8b608c5fed04e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1e8276b18a918834f4d",
+    "url": "/static/css/main.d8ae6f0e.chunk.css"
+  },
+  {
+    "revision": "736aebc16f9322c7c6dd",
+    "url": "/static/js/2.17f1e8bf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.17f1e8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e8276b18a918834f4d",
+    "url": "/static/js/main.8d6122e4.chunk.js"
+  },
+  {
+    "revision": "5fb11a1c23db92eab440",
+    "url": "/static/js/runtime-main.ebed117f.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "4a1cbb027477234e3977109f4469f671",
+    "url": "/static/media/logo.4a1cbb02.svg"
+  }
+]);
